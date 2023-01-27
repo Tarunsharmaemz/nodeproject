646236bfbc9b40c27e88707e167d606cd010c686
Tarunsharmaemz <tarun.sharma@emails.emizentech.com>
2023-01-27 12:17:16 +0530

    new changes

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -15,7 +15,7 @@ const userController = {
             'status' : req.body.status
         })
         
-        return res.json({ status : 200,userdata : user, message : 'User created successfully'})
+        return res.json({ status : 200,userdata : user, message : 'User has been created successfully'})
     },    
     findAll : async (req,res) => {    
         const users = await User.find();        
